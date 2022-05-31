896b57d00b60ff1d4b5c3c2c4946fc9b6ceefffa
hhvc <hectorvazquez.laboral@gmail.com>
2022-05-31 15:28:29 -0300

    TTS 31-05-2022

--- JS/js2.js
+++
@@ -1,9 +0,0 @@
-let marquesina=["Re","ci","cla","mos"];
-let i=0;
-let ubicacionDOM=document.getElementById("mjeReciclamos");
-function setI(){
-    if(i<3){i++}
-    else{i=0}
-    ubicacionDOM.innerHTML=marquesina[i];
-}
-setInterval(setI, 1000);
